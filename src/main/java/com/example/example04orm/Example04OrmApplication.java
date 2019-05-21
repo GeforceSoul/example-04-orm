387daf0bcefc5633e9edb33b48fd6015456473e9
a1181207784@163.com <1999418hhh>
2019-05-21 14:05:07 +0800

    Spring AOP切面实验

---
+++ src/main/java/com/example/example04orm/Example04OrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.example04orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example04OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example04OrmApplication.class, args);
+    }
+
+}
